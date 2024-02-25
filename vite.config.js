41469065a7390385e3a8fccaad22122ac99e8589
Jamie Portsmouth <jamports@mac.com>
2024-02-25 13:48:05 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/OpenPBR-glsl-example/"
+  base: "/OpenPBR-viewer/"
 })
